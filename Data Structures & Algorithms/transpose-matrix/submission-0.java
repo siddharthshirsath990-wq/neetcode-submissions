0e086767c8a01d678c738b3e76dc0f03e339b8aa
siddharthshirsath990-wq <siddharthshirsath990@gmail.com>
2026-09-04 21:48:45 +0530

    Add: transpose-matrix - submission-0

---
+++ Data Structures & Algorithms/transpose-matrix/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        
+       
+        int r=matrix.length;
+         int c=matrix[0].length;
+          int arr[][]=new int[c][r];
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr[j][i]=matrix[i][j];
+            }
+        }
+        return arr;
+        
+    }
+}
